35fc24a61f66aacc2aae65768c0142b47b78c789
mildredmeyah <105925894+mildredmeyah@users.noreply.github.com>
2024-05-17 13:46:53 +0200

    first commit

---
+++ user_input.py
@@ -0,0 +1,12 @@
+Name_string='mildred'
+Location='Phalaborwa, Limpopo, South Africa'
+Age='26'
+print(Name_string)
+
+print(Age )
+
+print(Location)
+
+
+print("my name is", Name_string,"i am ",Age,"years old","i live in ",Location)
+
